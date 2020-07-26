c88eb4e4b7e5306fa8edea9a306ba77f76b52f7a
karanjeetsingh2548 <68258787+karanjeetsingh2548@users.noreply.github.com>
2020-07-26 12:05:46 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request, redirect
 
-
 app=Flask(__name__)
 
 todos=[]
+  
 
-        
-@app.route("/",methods=["GET","POST"])
+
+@app.route("/")
 def index():
+    return render_template("index.html",todos=todos)
+
+@app.route("/tasker",methods=["GET","POST"])
+def add():
     if request.method == "GET":
-        return render_template("index.html",todos=todos)
+        return render_template("tasker.html")
     else:
         todo = request.form.get("task")
         todos.append(todo)
         return redirect("/")
+
